42ba632d18865410acfbce9288de5a619ed5ccca
Danyl Boiko <danyl.boiko_contractor@recordedfuture.com>
2023-02-26 17:35:36 +0100

    order created listener test

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -4,10 +4,22 @@ import {
   Subjects,
 } from "@danocto-tickets/common-tickets";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+
+    msg.ack();
+  }
 }
