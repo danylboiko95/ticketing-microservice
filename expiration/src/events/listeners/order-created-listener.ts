bda4e2c27187585631471a1306b53dd23c69ad33
Danyl Boiko <danyl.boiko_contractor@recordedfuture.com>
2023-02-28 12:40:06 +0100

    expiration added

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,20 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from "@danocto-tickets/common-tickets";
+import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
+}
